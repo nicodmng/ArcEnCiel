7333417805909947a089c1fb753df384e97b5e9d
NIcolas Demange <nicolas.demange@yahoo.fr>
2023-01-05 15:51:19 +0100

    SettingController builded

--- ArcEnCiel/AppDelegate.swift
+++ ArcEnCiel/AppDelegate.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UITabBarControllerDelegate {
 
 
 
@@ -30,7 +30,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if viewController is AddEventController {
+            if let newVC = tabBarController.storyboard?.instantiateViewController(withIdentifier: "AddEvent") {
+                tabBarController.present(newVC, animated: true)
+                return false
+            }
+        }
+        return true
+    }
 
 }
 
